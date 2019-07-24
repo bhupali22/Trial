d7d329bb3522c35bc9e6e7960c65ff3eb00ffd74
BMaheshwari <bhupali.maheshwari@gmail.com>
2019-07-24 15:42:58 +0530

    added styling

--- New/admin.py
+++ New/admin.py
@@ -3,7 +3,10 @@ from .models import Book
 # Register your models here.
 #we have to register individual model or app which we want to display in admin panel
 
-class BookAdmin(admin.ModelAdmin):
+
+# If you are happy with the default admin interface, you don’t need to define a ModelAdmin object at all – you can register the model class without providing a ModelAdmin description. i.e. admin.site.register(Book)
+
+class BookAdmin(admin.ModelAdmin):      #ModelAdmin class is created as we want custom values. Otherwise a default admin interface will be provided.
     list_display = ('title', 'author', 'price', 'stock')      #which fields to show in Books app
 
 
