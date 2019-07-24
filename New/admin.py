2eb8cc8b2defdafc57e87b2e9f24c0a0674f112b
BMaheshwari <bhupali.maheshwari@gmail.com>
2019-07-24 19:46:21 +0530

    Started with storefront

--- New/admin.py
+++ New/admin.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, Author
 # Register your models here.
 #we have to register individual model or app which we want to display in admin panel
 
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'price', 'stock')      #which fields to show in Books app
+    list_display = ('title', 'author', 'price', 'stock')      #which fields to show in Books app. Now we have defined author as foreign key so it will not display any fields in admin panel if we want to display author field. So we removed author field. After setting author field again manually we can add author entry here.
 
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name')
 
 admin.site.register(Book, BookAdmin)
+admin.site.register(Author, AuthorAdmin)
 #so we have registered model, now it will get display in admin page
--- New/models.py
+++ New/models.py
@@ -1,14 +1,31 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User     #import default user model
 # Create your models here.
 #djando itself create database and it interacts or talks with database through models. Model acts as a data layer. Usually single model maps with single database.
 
 #This is our store and we are going to sell books.
 
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    def __str__(self):      #whenever django asks who are you then it will return first name and last name. It helps in displaying value of Author field in admin panel
+        return "%s, %s" % (self.last_name, self.first_name)
+
+
 class Book(models.Model):
     title = models.CharField(max_length=200)
-    author = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.PROTECT)
     description = models.TextField()
     publish_date = models.DateField(default=timezone.now)
     price = models.DecimalField(decimal_places=2, max_digits=8)         #default value is added while migration as there are existing rows in database so they need to provide default value.
     stock = models.IntegerField(default=0)
+
+
+class Review(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.PROTECT)
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    publish_date = models.DateField(default=timezone.now)
+    text = models.TextField()
