4d3df4f077fac38674fa254ce1dac390d6edc92b
BMaheshwari <bhupali.maheshwari@gmail.com>
2019-07-25 19:52:49 +0530

    Added Cart ang Bookstore model and its views

--- New/views.py
+++ New/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from .models import Book
+from django.core.exceptions import ObjectDoesNotExist
+from django.shortcuts import render, redirect
+from .models import Book, BookOrder, Cart
 
 # Create your views here.
 def index(request): #request is an http request object which is passed to view as first argument. This request contains all the imediate information which django needs in order to provide response to browser.
@@ -23,3 +24,55 @@ def book_details(request, book_id):         #book_id will come from url
         'book': Book.objects.get(pk=book_id),
     }
     return render(request, 'New/details.html', context)
+
+def add_to_cart(request, book_id):
+    if request.user.is_authenticated():
+        try:
+            book = Book.objects.get(pk=book_id)
+        except ObjectDoesNotExist:
+            pass
+        else:
+            try:
+                cart = Cart.objects.get(user=request.user, active=True)
+            except ObjectDoesNotExist:
+                cart = Cart.objects.create(
+                    user=request.user
+                )
+                cart.save()
+            cart.add_to_cart(book_id)
+        return redirect('cart')
+    else:
+        return redirect('index')
+
+
+def remove_from_cart(request, book_id):
+    if request.user.is_authenticated():
+        try:
+            book = Book.objects.get(pk=book_id)
+        except ObjectDoesNotExist:
+            pass
+        else:
+            cart = Cart.objects.get(user=request.user, active=True)
+            cart.remove_from_cart(book_id)
+        return redirect('cart')
+    else:
+        return redirect('index')
+
+
+def cart(request):
+    if request.user.is_authenticated():
+        cart = Cart.objects.filter(user=request.user.id, active=True)
+        orders = BookOrder.objects.filter(cart=cart)
+        total = 0
+        count = 0
+        for order in orders:
+            total += (order.book.price * order.quantity)
+            count += order.quantity
+        context = {
+            'cart': orders,
+            'total': total,
+            'count': count,
+        }
+        return render(request, 'store/cart.html', context)
+    else:
+        return redirect('index')
